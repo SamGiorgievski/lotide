328dfa7d10bd3ce91f8cefd1203e4cbacd9a3a6c
Samuel Giorgievski <samgiorgievski@gmail.com>
2022-11-14 18:01:06 -0500

    built function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ 🌶 " + "Assertion passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌ 🤌 " + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 0);
