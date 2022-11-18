d95a3c3481148e5567031d9db4bfedad1737716e
Samuel Giorgievski <samgiorgievski@gmail.com>
2022-11-17 19:37:47 -0500

    created findKey.js

---
+++ findKey.js
@@ -0,0 +1,55 @@
+//assertEqual
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ 🌶 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ 🤌 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//findkey
+
+const findKey = function (object, callback) {
+  for (let x in object) {
+    if (callback(object[x])) {
+      return x;
+    }
+  }
+}
+
+
+//test 1
+console.log("Test 1:");
+
+assertEqual("noma", findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))
+
+//test 2
+console.log("Test 2:");
+
+assertEqual("Hello", findKey({
+  "Hi": 5,
+  "Hey": 10,
+  "Bye": 15,
+  "Goodbye": 20,
+  "Hello": 25,
+  "NoTy": 30
+}, x => x === 25))
+
+//test 3 - undefined
+console.log("Test 3:");
+
+assertEqual(undefined, findKey({
+  "Hi": 5,
+  "Hey": 10,
+  "Bye": 15,
+  "Goodbye": 20,
+  "Hello": 0,
+  "NoTy": 30
+}, x => x === 25))
