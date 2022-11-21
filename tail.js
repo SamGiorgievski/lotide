43904b7431aa7de725903357dac3d8152a7e524d
Samuel Giorgievski <samgiorgievski@gmail.com>
2022-11-21 14:09:30 -0500

    refactored tail.js and created tailTest

--- tail.js
+++ tail.js
@@ -1,14 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ 🌶 Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ 🤌 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 0);
-
 const tail = function(x) {
   let array = [];
   for (let i = 1; i < x.length; i++) {
@@ -17,11 +6,6 @@ const tail = function(x) {
   return array;
 };
 
-console.log(tail([3, 4, 5]));
+module.exports = tail;
 
-//TEST CASE
-const result = tail(["Hello", "Lighthouse", "Labs"]);// creating a new var to hold the array makes it easier to call in a test case!
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
-assertEqual(result[0], "Labs")
+
